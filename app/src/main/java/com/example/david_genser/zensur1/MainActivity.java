8f5bc9a5ad2ec656453b4888e448df2f50b282b2
David_GENSER <wgenser.wgenser@gmail.com>
2016-06-15 10:52:21 +0200

    10:52 15.Juni.2016 - Textfield

--- app/src/main/java/com/example/david_genser/zensur1/MainActivity.java
+++ app/src/main/java/com/example/david_genser/zensur1/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.david_genser.zensur1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TextView/EditText-Variablen erzeugen
+        final TextView Anzeige1 =
+                (TextView) findViewById(R.id.textView);
+
+        final EditText Eingabe1 =
+                (EditText) findViewById(R.id.editText);
+
+        //Button-Variablen erzeugen
+        Button OkButton = (Button) findViewById(R.id.button);
+
     }
 }
